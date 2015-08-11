2b9615285061867644392d10b41b7caa46a8977e
Kieron Richardson <artdude543@hotmail.co.uk>
2015-08-11 23:18:06 +0100

    Updated - Lang File Format / Loading

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // Get the required modules.
-var fs = require("fs");
+var fs = require("fs"), os = require("os"), _ = require('lodash');
 
+// Vairable to contain all the loaded locales.
 var langFiles = [];
-var list = fs.readdirSync('./lang/');
 
+// Read the lang directory to get all the lang files.
+var list = fs.readdirSync(__dirname + '/lang/');
+
+// Loop through all the lang files to process them so they can be used.
 for (var i = 0; i < list.length; i++) {
-  langFiles[list[i].replace('.json', '')] = require('./lang/' + list[i]);
+  var locale = list[i].replace('.txt', '');
+  langFiles[locale] = {};
+  fs.readFileSync(__dirname + '/lang/' + list[i]).toString('utf8').split(os.EOL).forEach(function (line) {
+    line = line.trim();
+    if (line.length > 0 && line.charAt(0) !== '#') {
+      var chunk = line.split('=');
+      langFiles[locale][chunk[0]] = chunk.slice(1).join('=');
+    }
+  });
 }
 
 module.exports = langFiles;
