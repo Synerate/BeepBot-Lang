f54c113d4aa43a2cc27f1bdc9e80cca015195283
Kieron Richardson <artdude543@hotmail.co.uk>
2015-11-15 16:34:41 +0000

    Changed - ES6 Upgrade

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-// Get the required modules.
-var fs = require("fs"), os = require("os"), _ = require('lodash');
+"use strict";
+
+const fs = require("fs");
+const os = require("os");
 
 // Variable to contain all the loaded locales.
-var langFiles = [];
-
+let langFiles = [];
 // Read the lang directory to get all the lang files.
-var list = fs.readdirSync(__dirname + '/lang/');
+let list = fs.readdirSync(__dirname + '/lang/');
 
 // Loop through all the lang files to process them so they can be used.
-for (var i = 0; i < list.length; i++) {
-  var locale = list[i].replace('.txt', '');
+for (let i = 0; i < list.length; i++) {
+  let locale = list[i].replace('.txt', '');
   langFiles[locale] = {};
   fs.readFileSync(__dirname + '/lang/' + list[i]).toString('utf8').split(os.EOL).forEach(function (line) {
     line = line.trim();
     if (line.length > 0 && line.charAt(0) !== '#') {
-      var chunk = line.split('=');
+      let chunk = line.split('=');
       langFiles[locale][chunk[0]] = chunk.slice(1).join('=');
     }
   });
