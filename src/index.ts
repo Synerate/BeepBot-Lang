8c94d37a92ed4c63a1a8223411c42e1e4908c7dd
Kieron Richardson <artdude543@hotmail.co.uk>
2016-10-07 20:47:38 +0100

    Fixed a derp with folder locations.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import * as fs from "fs";
 
 let langFiles: ILangFiles = {};
-let listing = fs.readdirSync(__dirname + "/lang/");
+let listing = fs.readdirSync(__dirname + "/../lang/");
 
 for (let i = 0, length = listing.length; i < length; i++) {
   let locale = listing[i].replace(".txt", "");
   langFiles[locale] = {};
-  let lines = fs.readFileSync(__dirname + "/lang/" + listing[i]).toString("utf8").split(/\r?\n/);
+  let lines = fs.readFileSync(__dirname + "/../lang/" + listing[i]).toString("utf8").split(/\r?\n/);
   for (let j = 0, lenj = lines.length; j < lenj; j++) {
     let line = lines[j].trim();
     if (line.length > 0 && line.charAt(0) !== "#") {
