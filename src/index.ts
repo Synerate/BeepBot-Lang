6157dbe26ee7fbe5fa271dc359ddad0c09229096
Kieron Richardson <artdude543@hotmail.co.uk>
2017-04-21 03:27:44 +0100

    Updated: New Lang! Also Improved It.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 import * as fs from "fs";
 
-let langFiles: ILangFiles = {};
-let listing = fs.readdirSync(__dirname + "/../lang/");
-
-for (let i = 0, length = listing.length; i < length; i++) {
-  let locale = listing[i].replace(".txt", "");
-  langFiles[locale] = {};
-  let lines = fs.readFileSync(__dirname + "/../lang/" + listing[i]).toString("utf8").split(/\r?\n/);
-  for (let j = 0, lenj = lines.length; j < lenj; j++) {
-    let line = lines[j].trim();
-    if (line.length > 0 && line.charAt(0) !== "#") {
-      let chunk = line.split("=");
-      langFiles[locale][chunk[0]] = chunk.slice(1).join("=");
-    }
-  }
-}
-
 export interface ILangFiles {
   [locale: string]: {
     [key: string]: string;
   };
+}
+
+const langFiles: ILangFiles = {};
+let listing = fs.readdirSync(`${__dirname}/../lang/`);
+
+for (let i = 0, length = listing.length; i < length; i++) {
+  let locale = listing[i].replace('.txt', '');
+  langFiles[locale] = {};
+  let lines = fs.readFileSync(__dirname + `/../lang/${listing[i]}`).toString('utf8').split(/\r?\n/);
+  for (let j = 0, lenj = lines.length; j < lenj; j++) {
+    let line = lines[j].trim();
+    if (line.length > 0 && line.charAt(0) !== '#') {
+      let chunk = line.split('=');
+      langFiles[locale][chunk[0]] = chunk.slice(1).join('=');
+    }
+  }
 }
 
 export { langFiles };
