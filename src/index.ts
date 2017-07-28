3ebf396aa12c149f1ab42bee01ac4bfbc354579f
Kieron Richardson <artdude543@hotmail.co.uk>
2017-07-28 13:24:36 +0100

    refactor(lang): change some of the default lang and add new ones.
    Change some of the default lang to read better.
    Remove the prefix of tagging a user as backend handles this part.
    Add new lang for the new commands/games/etc..

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ let listing = fs.readdirSync(`${__dirname}/../lang/`);
 for (let i = 0, length = listing.length; i < length; i++) {
   let locale = listing[i].replace('.txt', '');
   langFiles[locale] = {};
-  let lines = fs.readFileSync(__dirname + `/../lang/${listing[i]}`).toString('utf8').split(/\r?\n/);
+  let lines = fs.readFileSync(`${__dirname}/../lang/${listing[i]}`).toString('utf8').split(/\r?\n/);
   for (let j = 0, lenj = lines.length; j < lenj; j++) {
     let line = lines[j].trim();
     if (line.length > 0 && line.charAt(0) !== '#') {
